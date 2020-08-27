e47490fab7c3bdca894e5986668675a829ed283f
david mendoza <davidsmacbook@davids-mbp.attlocal.net>
2020-08-26 21:59:21 -0700

    updated server

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 
 const app = express();
+const server = http.createServer(app);
 
 //set static folder
-app.use(express.static(path.join(_dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
